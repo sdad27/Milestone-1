0991fb669209f59f4c173bf672266d4a8587a085
Saeed Ahmed <saeed.ahmed24696@gmail.com>
2024-11-10 14:41:38 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementById('toggle-skills')
+const skills = document.getElementById('skills')
+
+toggleButton.addEventListener('click', ()=> {
+    if(skills.style.display === 'none') {
+        skills.style.display = 'block'
+    } else {
+        skills.style.display = 'none'
+    }
+});
